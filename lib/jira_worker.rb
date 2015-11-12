63f4588b33c2456051eeb584e77d7362c5fc2397
Christian Chartron <Christian.Chartron@dako.de>
2015-11-12 18:25:45 +0100

    Jira transmission and comment

--- lib/jira_worker.rb
+++ lib/jira_worker.rb
@@ -1,5 +1,6 @@
 require 'jira'
 require 'pp'
+require 'yaml'
 
 CONSUMER_KEY = 'test'
 SITE         = 'https://testrest.atlassian.net/'
@@ -8,35 +9,44 @@ class JiraWorker
 	attr_accessor :client
 
 	def initialize
-		# credentials = YAML.load(File.read('credentials.yaml'))
-		#
-		# if credentials.values.any? {|v| v.eql?''}
-		#   raise "Set Login credentials in #{File.expand_path('credentials.yaml')}"
-		# end
-		#
-		# @client = JIRA::Client.new(
-		#     { :username => credentials[:username],
-		#       :password => credentials[:password],
-		#       :site => 'http://jira.dako.de',
-		#       :context_path => '',
-		#       :auth_type => :basic,
-		#       :use_ssl => false
-		#     })
+		config = YAML.load_file(File.expand_path('../jira.yaml'))
+
+		if config.values.any? {|v| v.eql?''}
+		  raise "Set Login credentials in #{}"
+		end
+
+		@client = JIRA::Client.new(
+		    { :username => config[:user],
+		      :password => config[:password],
+		      :site => config[:url],
+		      :context_path => '',
+		      :auth_type => :basic,
+		      :use_ssl => false
+		    })
 
 
-		options = {
-				:private_key_file => "rsakey.pem",
-				:context_path     => '',
-				:consumer_key     => CONSUMER_KEY,
-				:site             => SITE
-		}
+		# options = {
+		# 		:private_key_file => "rsakey.pem",
+		# 		:context_path     => '',
+		# 		:consumer_key     => CONSUMER_KEY,
+		# 		:site             => SITE
+		# }
+    #
+		# @client = JIRA::Client.new(options)
+		# access_token = @client.set_access_token('0U5MW7Bq4iZlZbhPRR3IE0F2D6O8VfKA', 'YBOiMI4ZHWMrlX87N1Dv3LnYQuNq1obL')
 
-		@client = JIRA::Client.new(options)
-		access_token = @client.set_access_token('0U5MW7Bq4iZlZbhPRR3IE0F2D6O8VfKA', 'YBOiMI4ZHWMrlX87N1Dv3LnYQuNq1obL')
 
-		project = @client.Project.find('TES')
+		#http://buildbot-1.dev.tachoweb.eu/logs/tachoweb-2015-03-02-15-25/top-build.log
+		project = @client.Project.find('TEST')
 
-		issue = @client.Issue.find('TES-1')
+
+		testcommitmsg = "Blie bla bulbsadas dadsad asd asd\n jira:TEST-55/done"
+
+		issue = @client.Issue.find('TEST-55')
+
+		issue.transitions.build.save("transition" => {"id" => '5'})
+
+		comment.save!(:body => "new comment")
 
 
 
@@ -44,6 +54,7 @@ class JiraWorker
 	end
 
 	def add_merged_commit commit
+
 	end
 
 	def add_failed_commit commit
@@ -52,7 +63,15 @@ class JiraWorker
 	private
 
 	def ticket_done(commit_message)
-		/jira:\s?(?<ticket>[\w]+-\d+)\/(?<operation>done|time)/
+		ticket = /jira:\s?(?<ticket>[\w]+-\d+)\/(?<operation>done|)/
+				.match(commit_message)
+		unless ticket[:ticket].empty?
+			msg = %(
+
+			)
+			issue = @client.Issue.find(ticket[:ticket])
+			issue.comment.save(:body => msg)
+		end
 	end
 
 	def ticket_merged(commit_message)
